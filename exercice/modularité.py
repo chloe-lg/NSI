cdc4b41c465212e90482e6eeaa4f7b63effda8b7
Chloé <cloclo.lg.277@gmail.com>
2024-09-26 11:58:34 +0200

    exo modu 3

--- exercice/modularité.py
+++ exercice/modularité.py
@@ -5,3 +5,18 @@ def contient(s,x):
     iEntier= x//64
     bit= x%64
     return (s[iEntier] & (1 << bit) !=0)
+
+def ajoute(s,x):
+    iEntier= x//64
+    bit= x%64
+    s[iEntier] = s[iEntier] | 1<<bit
+
+def enumere(s):
+    tab = []
+    a = 0
+    for e in s:
+        for i in range(64):
+            if e & (1 << i) !=0: 
+                tab.append(i + a*64)
+        a += 1
+    return tab
