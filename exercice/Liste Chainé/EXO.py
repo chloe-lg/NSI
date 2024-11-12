746e0013da66b6aaf079cf40a51917b7754eb765
Chloé <cloclo.lg.277@gmail.com>
2024-11-12 09:58:50 +0100

    nouv fichier

--- exercice/Liste Chainé/EXO.py
+++ exercice/Liste Chainé/EXO.py
@@ -24,15 +24,37 @@ def occurences_while(x, lst):
             return  1 + lst.suivante
         lst= lst.suivante
     return 0
-
-        
-
-    
-
-
-    
+  
 def concatener(l1, l2):
     if l1 is None:
         return l2
     else:
         return Cellule(l1.valuer, concatener(l1.suivante, l2))
+    
+def identique(l1, l2):
+    if l1 is None and l2 is None:
+        return True
+    
+    if l1 is None or l2 is None:
+        return False
+    
+    
+    if l1.valeur == l2.valeur:
+        return identique(l1.suivante, l2.suivante)
+
+    else:
+        return False
+
+l1 = Cellule(1, Cellule(2, Cellule(3, Cellule(4, None))))
+l2 = Cellule(1, Cellule(2, Cellule(3, Cellule(4, None))))
+
+c = identique(l1, l2)
+
+def inserer(x, lst):
+    if lst is None:
+        return Cellule(x, None)
+    elif lst.valeur >= x:
+        return Cellule(x, lst)
+    else:
+        return Cellule(lst.valeur, inserer(x, lst.suivnte))
+    
