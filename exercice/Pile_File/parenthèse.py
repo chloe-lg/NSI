d3b979ffbf41e63de6a89155d1a638e91fe52854
chloe-lg <135357371+chloe-lg@users.noreply.github.com>
2024-11-18 22:19:42 +0100

    Update parenthèse.py

--- exercice/Pile_File/parenthèse.py
+++ exercice/Pile_File/parenthèse.py
@@ -1,4 +1,36 @@
 from pile import Pile 
 
-def parenthese_correspondante(ep, i_fermante):
+def parenthese_correspondante(ep, f):
   p = cree_pile() 
+  i = 0  
+
+  while i < len(ep): 
+        if ep[i] == '(':
+            p.empiler(i)  
+        elif ep[i] == ')':
+            if p.est_vide():
+                raise ValueError("Expression mal parenthésée.")
+            indice_ouvrante = p.depiler() 
+            if i == f: 
+                return indice_ouvrante
+        i += 1  
+
+    raise ValueError("Indice de parenthèse fermante non valide ou expression mal formée.")
+  
+ep = "(a + (b * c) + (d / e))"
+
+try:
+    # Tests valides
+    assert parenthese_correspondante(ep, 7) == 3  
+    assert parenthese_correspondante(ep, 17) == 10  
+
+    # Test invalide
+    try:
+        parenthese_correspondante(ep, 0)  
+    except ValueError as e:
+        assert str(e) == "Indice de parenthèse fermante non valide ou expression mal formée."
+
+    print("Tous les tests sont passés !")
+
+except AssertionError:
+    print("Test a échoué.")
